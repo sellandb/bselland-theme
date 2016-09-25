68c7a7d994f9303c03ccb88ef5b8d9691e94a2a4
Ben Selland <sellandb@gmail.com>
2016-09-24 23:22:55 -0400

    Fixing Site Post Type

--- functions.php
+++ functions.php
@@ -1,9 +1,9 @@
 <?php
 
 function custom_settings_add_menu() {
-  add_menu_page( 'Custom Settings', 'Custom Settings', 'manage_options', 'custom-settings', 'custom_settings_page', null, 99);
+  add_menu_page('Custom Settings', 'Custom Settings', 'manage_options', 'custom-settings', 'custom_settings_page', null, 99);
 }
-add_action( 'admin_menu', 'custom_settings_add_menu' );
+add_action('admin_menu', 'custom_settings_add_menu' );
 
 // Create Custom Global Settings
 function custom_settings_page() { ?>
@@ -21,23 +21,17 @@ function custom_settings_page() { ?>
 
 add_theme_support( 'post-thumbnails' );
 
+//Custom Site Posts
 function create_site_post_type() {
-	register_post_type('site-post,
-			array(
-			'labels' => array(
-					'name' => __('Sites'),
-					'singular_name' => __('Site'),
-			),
-			'public' => true,
-			'has_archive' => true,
-			'supports' => array(
-					'title',
-					'editor',
-					'thumbnail',
-				  'custom-fields'
-			)
-	));
+  register_post_type('site-post,
+    array(
+      'labels' => array('name' => __('Sites'), 'singular_name' => __('Site')),
+      'public' => true,'has_archive' => true,
+      'supports' => array('title','editor','thumbnail','custom-fields')
+    )
+  );
 }
+
 add_action('init', 'create_site_post_type');
 
 ?>
