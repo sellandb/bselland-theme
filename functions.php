b9a9eb9d2142e377413a585e8c9f597509aca254
Ben Selland <sellandb@gmail.com>
2016-09-24 22:43:55 -0400

    Implement custom settings page

---
+++ functions.php
@@ -0,0 +1,18 @@
+function custom_settings_add_menu() {
+  add_menu_page( 'Custom Settings', 'Custom Settings', 'manage_options', 'custom-settings', 'custom_settings_page', null, 99);
+}
+add_action( 'admin_menu', 'custom_settings_add_menu' );
+
+// Create Custom Global Settings
+function custom_settings_page() { ?>
+  <div class="wrap">
+    <h1>Custom Settings</h1>
+    <form method="post" action="options.php">
+       <?php
+           settings_fields('section');
+           do_settings_sections('theme-options');
+           submit_button();
+       ?>
+    </form>
+  </div>
+<?php }
